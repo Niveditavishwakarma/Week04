46c722d5638f78c7c358cc4c6326d7337a42330e
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-02-13 12:55:16 +0530

    [Nivedita] Add . added the implementation of insurance policy management using hashmap

---
+++ Day02-collections/src/main/java/mapinterface/insurancepolicymanagementsystem/Main.java
@@ -0,0 +1,18 @@
+package mapinterface.insurancepolicymanagementsystem;
+import java.time.*;
+public class Main {
+    public static void main(String[] args) {
+        PolicyManager manager = new PolicyManager();
+
+        manager.addPolicy(new Policy("P1001", "Alice", LocalDate.now().plusDays(10), "Health", 500.0));
+        manager.addPolicy(new Policy("P1002", "Bob", LocalDate.now().plusDays(40), "Auto", 300.0));
+        manager.addPolicy(new Policy("P1003", "Charlie", LocalDate.now().minusDays(5), "Home", 700.0));
+
+        System.out.println("\nAll Policies: " + manager.getAllPolicies());
+        System.out.println("\nExpiring Soon: " + manager.getExpiringSoonPolicies());
+        System.out.println("\nPolicies of Alice: " + manager.getPoliciesByHolder("Alice"));
+
+        manager.removeExpiredPolicies();
+        System.out.println("\nAfter Removing Expired Policies: " + manager.getAllPolicies());
+    }
+}
