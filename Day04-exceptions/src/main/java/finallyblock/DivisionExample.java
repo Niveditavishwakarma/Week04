6888e161bd0bdf919b98d1df15fb17706b2c4a27
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-02-14 23:09:59 +0530

    [Nivedita] Add . added the demonstration of finally block

--- Day04-exceptions/src/main/java/finallyblock/DivisionExample.java
+++ Day04-exceptions/src/main/java/finallyblock/DivisionExample.java
@@ -1,23 +1,20 @@
 package finallyblock;
-
+import java.util.Scanner;
 public class DivisionExample {
     public static void main(String[] args) {
-        // Scanner to take user input
         Scanner scanner = new Scanner(System.in);
 
         try {
-            // Taking two integers as input
+
             System.out.print("Enter the numerator: ");
             int numerator = scanner.nextInt();
 
             System.out.print("Enter the denominator: ");
             int denominator = scanner.nextInt();
 
-            // Perform division and display the result
             int result = numerator / denominator;
             System.out.println("Result: " + result);
         } catch (ArithmeticException e) {
-            // Handle division by zero
             System.out.println("Error: Cannot divide by zero!");
         } finally {
             System.out.println("Operation completed");
